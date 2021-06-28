15374ce6bb5a68658de9c38cd902fd7de9154d06
motuko61 <ayaka19890122@gmail.com>
2021-06-28 14:13:30 +0900

    テストコード記述3

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,6 @@ class User < ApplicationRecord
 
   validates :data_of_birth, presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid' 
-
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid'
 end
