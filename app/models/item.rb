e31912d544ede7d7d8ffba200c7608284d3c35e7
motuko61 <ayaka19890122@gmail.com>
2021-06-28 16:25:26 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :user, presence: true
+  validates :name, presence: true
+  validates :category_id, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :condition_id, presence: true
+  validates :charge_id, presence: true
+  validates :area_id, presence: true
+  validates :days_to_ship_id, presence: true
   
 end
