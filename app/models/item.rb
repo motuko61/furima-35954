cd6570e1c7b7d71629733b1710857eb2822b3246
motuko61 <ayaka19890122@gmail.com>
2021-06-29 20:13:44 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,19 +7,23 @@ class Item < ApplicationRecord
   belongs_to :charge
   belongs_to :days_to_ship
   belongs_to :user
-
   has_one_attached :image
 
-
+  validates :image, presence: true
   validates :user, presence: true
   validates :name, presence: true
   validates :category_id, numericality: { other_than: 1 } 
-  validates :price, presence: true
   validates :description, presence: true
   validates :condition_id, numericality: { other_than: 1 } 
   validates :charge_id, numericality: { other_than: 1 } 
   validates :area_id, numericality: { other_than: 1 } 
   validates :days_to_ship_id, numericality: { other_than: 1 } 
 
+  with_options presence: true, numericality: { with: /\A[0-9]+\z/, message: 'is invalid' } do
+    validates :price
+    
+  end
+  
   
 end
+  
