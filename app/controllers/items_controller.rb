7f67ce71a9d9cf20fcc777d48d7979525b6c2a88
motuko61 <ayaka19890122@gmail.com>
2021-07-02 12:39:07 +0900

    情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -21,6 +21,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id 
+      redirect_to root_path
+    end  
+  end
+
+  def update
+    @item = Item.find(params[:id])
+     if @item.update(item_params)
+      redirect_to item_path
+     else
+      render :edit
+     end 
+  end  
+  
+  
   private
 
   def item_params
